4b9ffbe4fa46d071c1a9b4c35fbb7d3e65e5f672
Nicole WItherall <nicwitherall@gmail.com>
2021-05-18 16:57:54 -0400

    toggle smile emoji when mouseover 'name' div

--- app.js
+++ app.js
@@ -2,12 +2,16 @@
 
 let mouse = document.querySelector('.cursor');
 let tap = document.querySelector('.cursor-text');
+let burger = document.querySelector('.burger');
+let nameSpan = document.querySelector('.name');
+let smile = document.querySelector('.smile-img');
 
 
 // EVENT LISTENERS
 
 window.addEventListener('mousemove', cursorMovement);
 window.addEventListener('mouseover', cursorHover);
+burger.addEventListener('click', toggleNav);
 
 
 // FUNCTIONS
@@ -23,8 +27,16 @@ function cursorHover(e) {
     if (hovered.id == 'logo' || hovered.classList.contains('burger')) {
         mouse.classList.add('nav-active');
         tap.innerText = 'Tap';
+        smile.style.display = 'none';
+    } else if (hovered.classList.contains('name')){
+        smile.style.display = 'block';
     } else {
         mouse.classList.remove('nav-active');
         tap.innerText = '';
+        smile.style.display = 'none';
     }
+}
+
+function toggleNav(e) {
+
 }
