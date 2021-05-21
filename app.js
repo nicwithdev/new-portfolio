c4a9054c92c18c3cc1493845e802125b102bd50e
Nicole WItherall <nicwitherall@gmail.com>
2021-05-21 17:14:39 -0400

    finish contact page and mobile view

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ function cursorMovement(e) {
 
 function cursorHover(e) {
     const hovered = e.target;
-    if (hovered.id == 'logo' || hovered.classList.contains('burger') || hovered.classList.contains('link') || hovered.classList.contains("project-btn")) {
+    if (hovered.id == 'logo' || hovered.classList.contains('burger') || hovered.classList.contains('link') || hovered.classList.contains("project-btn") || hovered.classList.contains('back-to-top') || hovered.classList.contains("contact-link")) {
         mouse.classList.add('nav-active');
         tap.innerText = 'Tap';
         smile.style.display = 'none';
